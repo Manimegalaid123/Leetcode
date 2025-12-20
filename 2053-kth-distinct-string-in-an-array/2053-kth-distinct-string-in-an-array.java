8fb5b8bb96ec74a954ef728f2d690f8ac152171c
Manimegalai D <155413190+Manimegalaid123@users.noreply.github.com>
2025-12-20 18:01:10 +0530

    Time: 7 ms (54.6%), Space: 46.1 MB (59.91%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -4,16 +4,16 @@ class Solution {
         for(String s:arr){
             map.put(s,map.getOrDefault(s,0)+1);
         }
-        String st="";
+        
         int c=0;
-      for(int i=0;i<arr.length;i++){
-        if(map.get(arr[i])==1){
+      for(String s:arr){
+        if(map.get(s)==1){
             c=c+1;
             if(c==k){
-                st=arr[i];
+                return s;
             }
         }
       }
-      return st;
+      return "";
     }
 }
