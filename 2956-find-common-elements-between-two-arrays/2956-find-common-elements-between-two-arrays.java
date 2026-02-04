1476ad1db4f7850bb78bd67e240e1bd6a484d9da
Manimegalai D <155413190+Manimegalaid123@users.noreply.github.com>
2026-02-04 12:16:05 +0530

    Time: 7 ms (30.91%), Space: 47.2 MB (83.66%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
        HashSet<Integer> set=new HashSet<>() ;
-          HashSet<Integer> set1=new HashSet<>() ;
+         
        int c=0;
        int arr[]=new int[2];
        for(int num:nums2){
@@ -13,13 +13,14 @@ class Solution {
             c=c+1;
         }
        }
+       set.clear();
   int c1=0;
        arr[0]=c;
        for(int num:nums1){
-        set1.add(num);
+        set.add(num);
        }
        for(int num:nums2){
-  if(set1.contains(num)){
+  if(set.contains(num)){
     c1+=1;
   }
        }
