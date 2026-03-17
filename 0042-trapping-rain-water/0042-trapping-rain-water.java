cc8baf661f4bddaf889fc6a0b33fcb2aff4ce18a
Manimegalai D <155413190+Manimegalaid123@users.noreply.github.com>
2026-03-17 09:49:35 +0530

    Time: 1 ms (59.79%), Space: 48.2 MB (38.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int trap(int[] h) {
-        int n=h.length;
- int l[]=new int[n];
- int r[]=new int[n];
- int max=-1;
- for(int i=0;i<n;i++){
-    if(h[i]>=max){
-        max=h[i];
-    }
-    r[i]=max;
- }
- max=-1;
- for(int i=n-1;i>=0;i--){
-    if(h[i]>=max){
-        max=h[i];
-    }
-    l[i]=max;
- }
- int sum=0;
- for(int i=0;i<n;i++){
-    sum+=Math.min(l[i],r[i])-h[i];
- }
- return sum;
-    }
+    public int trap(int[] height) {
+        int max=-1;
+        int sum=0;
+       int l[]=new int[height.length];
+       int r[]=new int[height.length];
+       for(int i=0;i<height.length;i++){
+        if(height[i]>=max){
+            max=height[i];
 
+        }
+        l[i]=max;
+       } 
+       max=-1;
+       for(int i=height.length-1;i>=0;i--){
+        if(height[i]>=max){
+            max=height[i];
+        }
+        r[i]=max;
+       }
+       for(int i=0;i<height.length;i++){
+        sum+=Math.min(l[i],r[i])-height[i];
+       }
+       return sum;
+       
+    }
 }
