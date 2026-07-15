14551dc1f8ca96e0dbc4ea9019fc0324333376f9
Manimegalai D <155413190+Manimegalaid123@users.noreply.github.com>
2026-07-15 18:20:40 +0530

    Time: 5 ms (97.46%), Space: 43 MB (55.35%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,20 +1,20 @@
 class Solution {
     public int countSubstrings(String s) {
-        int c=0;
-        for(int i=0;i<s.length();i++) {
-           c=c+expand(s,i,i);
-           c=c+expand(s,i,i+1);
-
-        }
-        return c;
+int c=0;
+for(int i=0;i<s.length();i++){
+    c=c+expand(s,i,i);
+    c=c+expand(s,i,i+1);
+}
+return c;
     }
     public int expand(String s,int l,int r){
-        int count=0;
+        int c=0;
         while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
             l--;
             r++;
-            count=count+1;
+c++;
         }
-        return count;
+        return c;
+
     }
 }
